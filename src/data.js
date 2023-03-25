f00d7684b48dd11c8e5d5b349279fcbfc7f55033
Felipe Alves <felipealves3580@gmail.com>
2023-03-25 19:32:58 -0300

    Add data

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const faqData = [
+  {
+    id: 1,
+    pergunta: "Como entrar em contato?",
+    resposta:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quos tenetur facere cupiditate. Consectetur illo excepturi animi tempora, nobis, dolore perferendis aliquam aspernatur quaerat inventore vitae? Eaque tempore consectetur quam.",
+  },
+  {
+    id: 2,
+    pergunta: "Como acessar a fatura?",
+    resposta:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quos tenetur facere cupiditate. Consectetur illo excepturi animi tempora, nobis, dolore perferendis aliquam aspernatur quaerat inventore vitae? Eaque tempore consectetur quam.",
+  },
+  {
+    id: 3,
+    pergunta: "Qual o valor mensal da assinatura?",
+    resposta:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quos tenetur facere cupiditate. Consectetur illo excepturi animi tempora, nobis, dolore perferendis aliquam aspernatur quaerat inventore vitae? Eaque tempore consectetur quam.",
+  },
+];
